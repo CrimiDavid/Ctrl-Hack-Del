db84f7aeee315a8b2a5484f4294d8eace7c2c4c8
Matthew Frieri <mat.frieri@gmail.com>
2024-11-10 03:18:07 -0500

    datetimefield

--- backend/CHD/api/models.py
+++ backend/CHD/api/models.py
@@ -15,7 +15,7 @@ class Message(models.Model):
     content = models.TextField()
     from_user_id = models.ForeignKey(User, on_delete=models.CASCADE)
     to_conversation_id = models.ForeignKey(Conversation, on_delete=models.CASCADE)
-    timestamp = models.TimeField(auto_now_add=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return f"Message content: {self.content}"
