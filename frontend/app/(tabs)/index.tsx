223113f80c6a618ef5884c5a8e12d84ee2a709ba
WillSiddeley <will.iams.sapo@gmail.com>
2024-11-10 01:02:48 +0000

    fixes

--- frontend/app/(tabs)/index.tsx
+++ frontend/app/(tabs)/index.tsx
@@ -1,15 +1,60 @@
-import React from 'react';
-import MapView, { Region } from 'react-native-maps';
-import { StyleSheet, View } from 'react-native';
+import React, { Suspense } from 'react';
+import MapView, { Marker, Polygon, PROVIDER_GOOGLE, Region, Geojson } from 'react-native-maps';
+import { StyleSheet, View, Text } from 'react-native';
 import { useUser } from '~/lib/context/userContext';
 import { ActivityIndicator } from 'react-native';
 
-export default function App() {
+// This style removes most default map elements
+const BLANK_MAP_STYLE = [
+  {
+    featureType: 'all',
+    elementType: 'labels',
+    stylers: [
+      { visibility: 'on' }  // Removes all labels
+    ]
+  },
+  {
+    featureType: 'poi',
+    elementType: 'all',
+    stylers: [
+      { visibility: 'off' }  // Removes points of interest
+    ]
+  },
+  {
+    featureType: 'transit',
+    elementType: 'all',
+    stylers: [
+      { visibility: 'off' }  // Removes transit stations
+    ]
+  },
+  {
+    featureType: 'administrative',
+    elementType: 'all',
+    stylers: [
+      { visibility: 'on' }  // Removes administrative regions
+    ]
+  },
+  {
+    featureType: 'road',
+    elementType: 'labels',
+    stylers: [
+      { visibility: 'off' }  // Removes road labels
+    ]
+  }
+];
+
+export default function Map() {
   // Use hook to get the user
   const { user } = useUser();
-  console.log(user)
+  
   // Get the initial location from the user context
   const initialLocation = user?.location;
+
+  const mapRef = React.useRef<MapView>(null);
+
+  if (!initialLocation) {
+    return null;
+  }
 
   // If the user is loading the location, show a loading spinner
   if (user?.isLoadingLocation) {
@@ -23,9 +68,19 @@ export default function App() {
   return (
     <View style={styles.container}>
       <MapView
+        ref={mapRef}
+        provider={PROVIDER_GOOGLE}
         style={styles.map}
         initialRegion={initialLocation as Region}
-      />
+        customMapStyle={BLANK_MAP_STYLE}
+        showsPointsOfInterest={false}
+        showsBuildings={true}
+        showsTraffic={false}
+        showsIndoors={false}
+        showsMyLocationButton={false}
+      >
+
+      </MapView>
     </View>
   );
 }
