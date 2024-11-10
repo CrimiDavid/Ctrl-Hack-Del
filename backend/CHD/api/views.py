544387e705c8fd3a1325fd1952678d12099e373e
Matthew Frieri <mat.frieri@gmail.com>
2024-11-10 02:34:55 -0500

    bug fix

--- backend/CHD/api/views.py
+++ backend/CHD/api/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import generics, status
-refrom .models import Location, User, Message, Conversation
+from .models import Location, User, Message, Conversation
 from .serializers import UserSerializer, MessageSerializer, ConversationSerializer
 from django.contrib.auth import authenticate
 
