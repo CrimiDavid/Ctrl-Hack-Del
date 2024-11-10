98e4bc9137b6414cb3e0a597d746c9e1ccc4f965
Matthew Frieri <mat.frieri@gmail.com>
2024-11-09 23:13:42 -0500

    added /getConversationMessages endpoint

--- backend/CHD/api/urls.py
+++ backend/CHD/api/urls.py
@@ -4,7 +4,6 @@ from django.urls import path
 
 urlpatterns = [
     path('listUsers/<str:id>/', views.ListUsersView.as_view(), name=''),
-    path('api/sendMessage/', views.SendMessageView.as_view(), name='')
-
-    
+    path('sendMessage/', views.SendMessageView.as_view(), name=''),
+    path('getConversationMessages/<str:id>', views.GetConversationMessagesView.as_view(), name='')
 ]
